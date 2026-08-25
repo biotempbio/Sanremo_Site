3dc1f2e304676b43ba8d0c7a98c46e984e746e0b
biotempbio <biotempbio@gmail.com>
2026-08-25 13:45:04 +0300

    Use local PT typography and sharpen brand lockup

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,12 +53,6 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="ru">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&family=Playfair+Display:wght@700&family=Roboto:wght@400;500;700&display=swap"
-        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(ORG_SCHEMA) }}
